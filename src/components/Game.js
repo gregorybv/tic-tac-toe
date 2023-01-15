476f0c349e8a643274c76353a4a9d3686f9e1fc7
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-01-16 01:08:37 +0300

    logic app 2

--- src/components/Game.js
+++ src/components/Game.js
@@ -4,6 +4,12 @@ import {calculateNewValue} from "@testing-library/user-event/dist/utils";
 
 const reducer = (state, action) => {
   switch (action.type) {
+    case 'JUMP':
+      return {
+        ...state,
+        xIsNext: action.payload.step % 2 === 0,
+        history: state.history.slice(0, action.payload.step + 1),
+      }
     case 'MOVE':
       return {
         ...state, history: state.history.concat({
@@ -22,9 +28,12 @@ export default function Game() {
     history: [{squares: Array(9).fill(null)}],
   })
   const {xIsNext, history} = state
+  const jumpTo = (step) => {
+    dispatch({type: 'JUMP', payload: {step}})
+  }
   const handleClick = (i) => {
     const current = history[history.length - 1]
-    const squares = current.slice()
+    const squares = current.squares.slice()
     const winner = calculateWinner(squares)
     if (winner || squares[i]) {
       return
@@ -34,21 +43,27 @@ export default function Game() {
   }
   const current = history[history.length - 1]
   const winner = calculateWinner(current.squares)
-  const status = winner ? winner === 'D' ? 'Draw' : 'Winner is' + winner :
-    'Next player is ' + (xIsNext ? 'X' : 'O')
-:
+  const status = winner
+    ? winner === 'D'
+      ? 'Draw'
+      : 'Winner is ' + winner
+    : 'Next player is ' + (xIsNext ? 'X' : 'O')
 
   const moves = history.map((step, move) => {
     const desc = move ? 'Go to #' + move : 'Start the Game'
-    return <li>
-      <button>{desc}</button>
-    </li>
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>
+          {desc}
+        </button>
+      </li>
+    )
   })
-  const squares = Array(9).fill(null)
+
   return (
     <div className="game">
       <div className="game-board">
-        <Board squares={squares}></Board>
+        <Board onClick={(i) => handleClick(i)} squares={current.squares}></Board>
       </div>
       <div className="game-info">
         <div>{status}</div>
@@ -58,6 +73,27 @@ export default function Game() {
   )
 }
 const calculateWinner = (squares) => {
+  const winnerLines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 8]
+  ]
+  let isDraw = true
+  for (let i = 0; i < winnerLines.length; i++) {
+    const [a,b,c] = winnerLines[i]
+    if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+      return squares[a]
+    }
+    if (!squares[a] || !squares[b] || !squares[c]) {
+      isDraw = false
+    }
+  }
+  if (isDraw) return 'D'
   return null
 }
 
