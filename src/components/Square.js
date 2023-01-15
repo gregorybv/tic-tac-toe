0344649e4328085088b5ce5f1ef6ae1abeda6ab9
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-01-15 23:30:11 +0300

    style

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
-export default function Square() {
-  return <button></button>
+export default function Square(props) {
+  return <button className="btn" onClick={props.onClick}>
+    {props.value}
+  </button>
 } 
 
 
