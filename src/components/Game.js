d664fd4cf33d1cc45e441910ea76f394f8327f42
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-01-16 00:20:40 +0300

    logic app

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,21 +1,64 @@
-import React from 'react';
+import React, {useReducer} from 'react';
 import Board from "./Board";
+import {calculateNewValue} from "@testing-library/user-event/dist/utils";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'MOVE':
+      return {
+        ...state, history: state.history.concat({
+          squares: action.payload.squares,
+        }),
+        xIsNext: !state.xIsNext,
+      }
+    default:
+      return state
+  }
+}
 
 export default function Game() {
-  const status = "Next player is X"
-  const moves = <li>
-    <button>Start the Game</button>
-  </li>
+  const [state, dispatch] = useReducer(reducer, {
+    xIsNext: true,
+    history: [{squares: Array(9).fill(null)}],
+  })
+  const {xIsNext, history} = state
+  const handleClick = (i) => {
+    const current = history[history.length - 1]
+    const squares = current.slice()
+    const winner = calculateWinner(squares)
+    if (winner || squares[i]) {
+      return
+    }
+    squares[i] = xIsNext ? 'x' : 'o'
+    dispatch({type: 'MOVE', payload: {squares}})
+  }
+  const current = history[history.length - 1]
+  const winner = calculateWinner(current.squares)
+  const status = winner ? winner === 'D' ? 'Draw' : 'Winner is' + winner :
+    'Next player is ' + (xIsNext ? 'X' : 'O')
+:
+
+  const moves = history.map((step, move) => {
+    const desc = move ? 'Go to #' + move : 'Start the Game'
+    return <li>
+      <button>{desc}</button>
+    </li>
+  })
   const squares = Array(9).fill(null)
-  return <div>
-    <div className="game-board">
-      <Board squares={squares}></Board>
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board squares={squares}></Board>
+      </div>
+      <div className="game-info">
+        <div>{status}</div>
+        <div>{moves}</div>
+      </div>
     </div>
-    <div className="game-info">
-      <div>{status}</div>
-      <div>{moves}</div>
-    </div>
-  </div>
+  )
+}
+const calculateWinner = (squares) => {
+  return null
 }
 
 
